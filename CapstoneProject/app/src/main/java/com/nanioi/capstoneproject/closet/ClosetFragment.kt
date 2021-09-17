8f7f3ac52759b50ff6ce172f7e4830cb871a3406
Nanioi <0802ny@gmail.com>
2021-09-17 19:21:09 +0900

    기본적인 UI 생성
    - BottomNavigationView menu 생성
    - Fragment 생성
    - 로그인, 회원가입 창 생성

---
+++ CapstoneProject/app/src/main/java/com/nanioi/capstoneproject/closet/ClosetFragment.kt
@@ -0,0 +1,7 @@
+package com.nanioi.capstoneproject.closet
+
+import androidx.fragment.app.Fragment
+import com.nanioi.capstoneproject.R
+
+class ClosetFragment: Fragment(R.layout.fragment_closet) {
+}
