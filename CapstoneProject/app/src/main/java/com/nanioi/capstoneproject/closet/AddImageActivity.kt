bd83767ac0dc0a29d20b3f28f64898a7878203c6
Nanioi <0802ny@gmail.com>
2021-09-18 19:47:26 +0900

    Ui 구성 ( 21.09.18 나연 )
    - 아이템 추가 activity  ui 생성
    - 옷장 탭 +버튼 추가
    - 홈 탭 아바타, 버튼 추가
    - 마이페이지 프로필이미지, 얼굴&전신 사진 추가 부분, 아바타생성버튼 추가

---
+++ CapstoneProject/app/src/main/java/com/nanioi/capstoneproject/closet/AddImageActivity.kt
@@ -0,0 +1,11 @@
+package com.nanioi.capstoneproject.closet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_image)
+    }
+}
