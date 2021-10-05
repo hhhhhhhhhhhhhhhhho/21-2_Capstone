5d2cc464317a05d5959a77a2ccd2b65eaef659ce
Nanioi <0802ny@gmail.com>
2021-10-05 18:00:05 +0900

    회원가입, 프로필 수정 UI 수정 ( 21.10.05 나연 )
    - 성별 라디오 버튼 추가
    - 스크롤 뷰로 변경

--- CapstoneProject/app/src/main/java/com/nanioi/capstoneproject/mypage/EditProfileActivity.kt
+++ CapstoneProject/app/src/main/java/com/nanioi/capstoneproject/mypage/EditProfileActivity.kt
@@ -16,6 +16,8 @@ class EditProfileActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //TODO 준승님이 해야할 것 각각 EditText 부분에 회원정보 불러서 setText 시키기
+        // 즉 클릭 전에 이미 저장된 자신의 정보들이 칸마다 정보가 불러와 져있고 화면에 그게 떠야 합니다.
+        // 클릭해서 정보 입력하면 그 정보들은 그 프로필 수정 버튼이 눌려야 저장된 정보가 바뀌게 구현해주세요
 
     }
 }
