bc28787770c076082fe1ab8373e446bef6a65957
proggg <xowjdpk@gmail.com>
2021-11-25 17:05:05 +0900

    release commit

--- ClosetApplication/app/src/main/java/com/nanioi/closetapplication/DBkey.kt
+++
@@ -1,9 +0,0 @@
-package com.nanioi.closetapplication
-
-class DBkey {
-    companion object{
-        const val DB_USERS = "Users"
-        const val DB_ITEM = "Items"
-        const val DB_SELECTED_ITEM = "SelectedItem"
-    }
-}
