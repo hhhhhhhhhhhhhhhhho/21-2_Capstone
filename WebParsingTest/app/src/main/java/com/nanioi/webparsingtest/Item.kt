885bac3415b53ea672c72d6c042a5bce4424b03e
Nanioi <0802ny@gmail.com>
2021-11-26 23:45:49 +0900

    merge

--- WebParsingTest/app/src/main/java/com/nanioi/webparsingtest/Item.kt
+++
@@ -1,7 +0,0 @@
-package com.nanioi.webparsingtest
-
-data class Item(
-    val title: String,
-    val url:String,
-    val thumb: String
-)
--- WebParsingTest/app/src/main/java/com/nanioi/webparsingtest/URLDataBase.kt
+++
@@ -1,4 +0,0 @@
-package com.nanioi.webparsingtest
-
-class URLDataBase {
-}
